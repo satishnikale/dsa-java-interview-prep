8303830d3747b7df275746a61c03f44e2bf1a5be
Satish Nikale <satishnikale7@gmail.com>
2026-05-15 22:27:07 +0530

    Add CountSum class to calculate and display the sum of two numbers

---
+++ basics/CountSum.java
@@ -0,0 +1,12 @@
+class CountSum {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int totalSum = addNumbers(a, b);
+        System.out.println("Total is : " + totalSum);
+    }
+}
