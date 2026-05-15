3f17a890aea1e53000bcc0cb2746a7b34b446b23
Satish Nikale <satishnikale7@gmail.com>
2026-05-15 23:05:54 +0530

    Add overloaded addNumbers method to support three integer parameters

--- basics/CountSum.java
+++ basics/CountSum.java
@@ -2,6 +2,10 @@ class CountSum {
     public static int addNumbers(int a, int b) {
         return a + b;
     }
+
+     public static int addNumbers(int a, int b, int c) {
+        return a + b + c;
+    }
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
